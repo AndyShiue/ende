c81d5a87e930601df0dc1ba1711e445c867567b5
petercommand <petercommand@gmail.com>
2016-05-23 17:57:05 +0800

    remove warnings

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -2,9 +2,11 @@ extern crate llvm_sys;
 
 pub mod ast;
 pub mod compile;
+#[allow(dead_code, non_camel_case_types, non_snake_case)]
 pub mod Ast {
     include!("../../frontend/Ast.rs");
 }
+#[allow(dead_code, non_camel_case_types, non_snake_case)]
 pub mod Rts {
     include!("../../frontend/rts.rs");
 }
