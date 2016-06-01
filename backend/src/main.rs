e14f07c9bda3e211c324f73407f97a520271e5a6
Andy Shiue <andyshiue851019@gmail.com>
2016-06-01 12:07:03 +0800

    Remove unused code and fix parsing for integers.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -16,23 +16,8 @@ unsafe fn haskell_exit() {
 }
 
 pub fn main() {
-    use ende::ast::*;
-    use ende::ast::Operator::*;
-    use ende::ast::Term::*;
-    use ende::ast::Statement::*;
-    use ende::type_check::Type::*;
     use ende::codegen::*;
     use ende::trans::*;
-    /*
-    let stmt = LetMut("count".to_string(), Literal(10));
-    let cond_block = Block { stmts: vec![Mutate("count".to_string(), infix!(Var("count".to_string()), Sub, Literal(1)))],  end: Box::new(Var("count".to_string())) };
-    let cond_term = Scope(cond_block);
-    let a = vec![Var("count".to_string())];
-    let inner_block = Block { stmts: vec![], end: Box::new(Call(FunctionCall { name: "print".to_string() }, a )) };
-    let args_types = FunctionTy(vec![I32Ty], Box::new(I32Ty));
-    let stmts = vec![Extern("print".to_string(), args_types), stmt];
-    let stmt = Scope(Block { stmts: stmts, end: Box::new(While(Box::new(cond_term), inner_block)) });
-    */
     unsafe {
         haskell_init();
         let tree_prim = ende::Parsing::getTree();
