75589b63e02cc4ddb584e47bc6d7d5cc2d8b920c
petercommand <petercommand@gmail.com>
2016-06-04 06:24:46 +0800

    fix backend/build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 llvm-config --version >/dev/null 2>/dev/null
-if [ "$!" == 0 ]; then
+if [ "$?" == 0 ]; then
 	cargo build
 else
 	PATH=$(dirname $(locate llvm-config |grep bin|head -n 1)):$PATH cargo build
