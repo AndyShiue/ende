cd620ee7cb75f6b833ab4af543e2a3e66f4c7ec6
petercommand <petercommand@gmail.com>
2016-06-04 11:23:41 +0800

    fix build system

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash -e
+build="0"
 if [ ! -e "last_build" ]; then
+    build="1"
+fi
+
+
+if [ $build == "1" ]; then
     rm -f last_build
+    stack clean
     stack build 2> log || (cat log && false)
     ./get_args.sh
     stack exec ghc -- src/Ast.hs src/Parsing.hs && ar rcs libParsing.a src/Parsing.o src/Ast.o
     ./compile_closure_func.sh
     ./bind.sh
     touch last_build
+else
+    make lastcomplete
 fi
