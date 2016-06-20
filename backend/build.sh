929931551e685d2d793a0aaebb700a062ce5e0a0
Petercommand <petercommand@gmail.com>
2016-06-20 17:29:06 +0800

    use brew --prefix instead of locate

--- backend/build.sh
+++ backend/build.sh
@@ -3,5 +3,5 @@ llvm-config --version >/dev/null 2>/dev/null
 if [ "$?" == 0 ]; then
 	cargo build
 else
-	PATH=$(dirname $(locate llvm-config |grep bin|head -n 1)):$PATH cargo build
+	PATH=$(brew --prefix llvm)/bin:$PATH cargo build
 fi
